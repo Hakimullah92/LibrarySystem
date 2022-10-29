6e9504ebe9ca0943c46d6f6c9cd8d2ddcd311a4f
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-10-29 12:47:47 +0500

    Making Hero Section with image slider

---
+++ hero.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hero</title>
+
+    <!-- -------------- Bootstrap CSS File ---------------- -->
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+
+    <!-- ----------------- Custom CSS File -------------------- -->
+
+    <link rel="stylesheet" href="CSS/hero.css">
+
+</head>
+<body>
+    <main>
+        <div class="container-fluid p-0">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                 <div class="carousel-inner">
+                     <div class="carousel-item active">
+                      <img src="./images/Hero/Three.jpg" class="d-block w-100" alt="...">
+                       </div>
+                <div class="carousel-item">
+                   <img src="./images/Hero/one.jpg" class="d-block w-100" alt="...">
+                       </div>
+    <div class="carousel-item">
+      <img src="./images/Hero/four.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+  </div>
+    </main>
+    <!-- ----------------- Bootstrap JS File ---------------- -->
+    <script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
